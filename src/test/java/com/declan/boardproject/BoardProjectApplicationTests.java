ce477ce2bec50a687de4c831e88ca46bc02fcafb
declan <41declan4@gmail.com>
2022-08-08 22:04:21 +0900

    스프링 부트 시작하기

---
+++ src/test/java/com/declan/boardproject/BoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.declan.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
